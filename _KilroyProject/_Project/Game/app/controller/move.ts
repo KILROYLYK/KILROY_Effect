9ee9af3e94aee99530a42fd18c56ae74d0f0039b
kilroy_lyk <736587340@qq.com>
2020-04-16 18:37:58 +0800

    KILROY

--- _KilroyProject/_Project/Game/app/controller/move.ts
+++ _KilroyProject/_Project/Game/app/controller/move.ts
@@ -6,7 +6,6 @@ import Global from '../constant/global';
 export default class Move {
     private readonly config: object = { // 配置
         target: null, // 目标对象
-        far: 300, // 目标对象视距
         lon: 90, // 经度
         lat: 0, // 纬度
         theta: 0, // 角度
@@ -70,9 +69,9 @@ export default class Move {
         _this.config.lat = Math.max(-85, Math.min(85, _this.config.lat));
         _this.config.phi = Global.THREE.Math.degToRad(90 - _this.config.lat);
         _this.config.theta = Global.THREE.Math.degToRad(_this.config.lon);
-        _this.config.target.x = Math.sin(_this.config.phi) * Math.cos(_this.config.theta) * _this.config.far;
-        _this.config.target.y = Math.cos(_this.config.phi) * _this.config.far;
-        _this.config.target.z = Math.sin(_this.config.phi) * Math.sin(_this.config.theta) * _this.config.far;
+        _this.config.target.x = Math.sin(_this.config.phi) * Math.cos(_this.config.theta) * _this.camera.far;
+        _this.config.target.y = Math.cos(_this.config.phi) * _this.camera.far;
+        _this.config.target.z = Math.sin(_this.config.phi) * Math.sin(_this.config.theta) * _this.camera.far;
         _this.camera.lookAt(_this.config.target);
     }
     
@@ -101,65 +100,67 @@ export default class Move {
      * @return {void}
      */
     clickMoveCamera() {
-        const _this = this;
+        const _this = this,
+            D = Global.Document,
+            mouse = {
+                /**
+                 * 鼠标按下
+                 * @param {object} e 焦点对象
+                 * @return {void}
+                 */
+                down: (e): void => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    
+                    D.addEventListener('mousemove', mouse.move, false);
+                    D.addEventListener('mouseup', mouse.up, false);
+                },
+                
+                /**
+                 * 鼠标移动
+                 * @param {object} e 焦点对象
+                 * @return {void}
+                 */
+                move: (e): void => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    
+                    const movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0,
+                        movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0,
+                        speed = _this.camera.fov * _this.config.speed.click;
+                    
+                    _this.config.lon -= movementX * speed;
+                    _this.config.lat += movementY * speed;
+                },
+                
+                /**
+                 * 鼠标抬起
+                 * @param {Event} e 事件对象
+                 * @return {void}
+                 */
+                up: (e): void => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    
+                    D.removeEventListener('mousemove', mouse.move, false);
+                    D.removeEventListener('mouseup', mouse.up, false);
+                },
+                
+                /**
+                 * 鼠标中键移动
+                 * @param {object} e 焦点对象
+                 * @return {void}
+                 */
+                wheel: (e): void => {
+                    const fov = _this.camera.fov + e.deltaY * _this.config.speed.wheel;
+                    
+                    _this.camera.fov = Global.THREE.Math.clamp(fov, 45, 95);
+                    _this.camera.updateProjectionMatrix();
+                }
+            };
         
-        Global.Document.addEventListener('mousedown', onMouseDown, false);
-        Global.Document.addEventListener('wheel', onMouseWheel, false);
-        
-        /**
-         * 鼠标按下
-         * @param {object} e 焦点对象
-         * @return {void}
-         */
-        function onMouseDown(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            
-            Global.Document.addEventListener('mousemove', onMouseMove, false);
-            Global.Document.addEventListener('mouseup', onMouseUp, false);
-        }
-        
-        /**
-         * 鼠标移动
-         * @param {object} e 焦点对象
-         * @return {void}
-         */
-        function onMouseMove(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            
-            const movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0,
-                movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0,
-                speed = _this.camera.fov * _this.config.speed.click;
-            
-            _this.config.lon -= movementX * speed;
-            _this.config.lat += movementY * speed;
-        }
-        
-        /**
-         * 鼠标抬起
-         * @param {object} e 焦点对象
-         * @return {void}
-         */
-        function onMouseUp(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            
-            Global.Document.removeEventListener('mousemove', onMouseMove);
-            Global.Document.removeEventListener('mouseup', onMouseUp);
-        }
-        
-        /**
-         * 鼠标中键移动
-         * @param {object} e 焦点对象
-         * @return {void}
-         */
-        function onMouseWheel(e) {
-            const fov = _this.camera.fov + e.deltaY * _this.config.speed.wheel;
-            
-            _this.camera.fov = Global.THREE.Math.clamp(fov, 45, 95);
-            _this.camera.updateProjectionMatrix();
-        }
+        D.addEventListener('mousedown', mouse.down, false);
+        D.addEventListener('wheel', mouse.wheel, false);
     }
     
     /**
@@ -167,55 +168,57 @@ export default class Move {
      * @return {void}
      */
     touchMoveCamera() {
-        const _this = this;
+        const _this = this,
+            D = Global.Document,
+            touch = {
+                /**
+                 * 触摸开始
+                 * @param {object} e 焦点对象
+                 * @return {void}
+                 */
+                start: (e): void => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    
+                    const touch = e.touches[0];
+                    _this.config.position.touchX = touch.screenX;
+                    _this.config.position.touchY = touch.screenY;
+                    
+                    D.addEventListener('touchmove', touch.move, false);
+                    D.addEventListener('touchend', touch.end, false);
+                },
+                
+                /**
+                 * 触摸移动
+                 * @param {object} e 焦点对象
+                 * @return {void}
+                 */
+                move: (e): void => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    
+                    const touch = e.touches[0];
+                    _this.config.lon -= (touch.screenX - _this.config.position.touchX) * _this.config.speed.touch;
+                    _this.config.lat += (touch.screenY - _this.config.position.touchY) * _this.config.speed.touch;
+                    
+                    _this.config.position.touchX = touch.screenX;
+                    _this.config.position.touchY = touch.screenY;
+                },
+                
+                /**
+                 * 触摸抬起
+                 * @param {object} e 焦点对象
+                 * @return {void}
+                 */
+                end: (e): void => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    
+                    D.removeEventListener('touchmove', touch.move, false);
+                    D.removeEventListener('touchend', touch.end, false);
+                }
+            };
         
-        Global.Document.addEventListener('touchstart', onTouchStart, false);
-        
-        /**
-         * 触摸开始
-         * @param {object} e 焦点对象
-         * @return {void}
-         */
-        function onTouchStart(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            
-            const touch = e.touches[0];
-            _this.config.position.touchX = touch.screenX;
-            _this.config.position.touchY = touch.screenY;
-            
-            Global.Document.addEventListener('touchmove', onTouchMove, false);
-            Global.Document.addEventListener('touchend', onTouchEnd, false);
-        }
-        
-        /**
-         * 触摸移动
-         * @param {object} e 焦点对象
-         * @return {void}
-         */
-        function onTouchMove(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            
-            const touch = e.touches[0];
-            _this.config.lon -= (touch.screenX - _this.config.position.touchX) * _this.config.speed.touch;
-            _this.config.lat += (touch.screenY - _this.config.position.touchY) * _this.config.speed.touch;
-            
-            _this.config.position.touchX = touch.screenX;
-            _this.config.position.touchY = touch.screenY;
-        }
-        
-        /**
-         * 触摸抬起
-         * @param {object} e 焦点对象
-         * @return {void}
-         */
-        function onTouchEnd(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            
-            Global.Document.removeEventListener('touchmove', onTouchMove);
-            Global.Document.removeEventListener('touchend', onTouchEnd);
-        }
+        D.addEventListener('touchstart', touch.start, false);
     }
 }
