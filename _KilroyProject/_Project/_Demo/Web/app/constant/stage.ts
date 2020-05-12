ecbdf6875c7a97f4804ba43b9d8a55e2c3dabc59
kilroy_lyk <736587340@qq.com>
2020-05-12 15:34:11 +0800

    KILROY

--- _KilroyProject/_Project/_Demo/Web/app/constant/stage.ts
+++ _KilroyProject/_Project/_Demo/Web/app/constant/stage.ts
@@ -1,8 +1,10 @@
+import Anime from '../stage/anime/stage';
 import Particle from '../stage/particle/stage';
 
 /**
  * Stage
  */
 export default class Stage {
+    readonly static Anime: Function = Anime; // 动画
     readonly static Particle: Function = Particle; // 粒子
 }
--- _KilroyProject/_Project/_Demo/Web/app/stage/anime/stage.ts
+++ _KilroyProject/_Project/_Demo/Web/app/stage/anime/stage.ts
@@ -1,6 +1,7 @@
 import Global from '../../constant/global';
 import _Stage from '../../interface/stage';
-import Particle from './particle';
+
+import Anime, { set } from 'animejs';
 
 /**
  * 场景
@@ -9,7 +10,6 @@ export default class Stage implements _Stage {
     public readonly config: object = { // 配置
         dom: null as Element, // 元素
     };
-    private particle = null as Particle // 粒子对象
     
     /**
      * 构造函数
@@ -29,9 +29,8 @@ export default class Stage implements _Stage {
      * @return {any} 实例
      */
     private create(): void {
-        const _this = this;
-        
-        _this.particle = new Particle(_this.config.dom);
+        const _this = this,
+            dom = Global.Document.createElement('div');
     }
     
     /**
@@ -40,7 +39,7 @@ export default class Stage implements _Stage {
      */
     private init(): void {
         const _this = this;
-        _this.particle.writeText('♥');
+        
     }
     
     /**
@@ -51,11 +50,6 @@ export default class Stage implements _Stage {
     public update(isResize: boolean = false): void {
         const _this = this;
         
-        _this.particle && _this.particle.update();
-        
-        if (isResize) {
-            _this.particle && _this.particle.update(true);
-        }
     }
     
     /**
@@ -64,7 +58,5 @@ export default class Stage implements _Stage {
      */
     public destroy(): void {
         const _this = this;
-        _this.particle.destroy();
-        _this.particle = null;
     }
 }
