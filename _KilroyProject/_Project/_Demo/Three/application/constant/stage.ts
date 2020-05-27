ab096ab04de5b1381d8c80b5475da115da978ec3
kilroy_lyk <736587340@qq.com>
2020-05-27 11:00:12 +0800

    KILROY

--- _KilroyProject/_Project/_Demo/Three/application/constant/stage.ts
+++ _KilroyProject/_Project/_Demo/Three/application/constant/stage.ts
@@ -1,7 +1,7 @@
 // import GlobalStage from '../module/Display/stage'; // 展示
 // import GlobalStage from '../module/Panoramic/stage'; // 全景
 // import GlobalStage from '../module/Flight/stage'; // 飞行
-import GlobalStage from '../module/Universe/stage'; // 宇宙
+import GlobalStage from '../module/Solar/stage'; // 太阳系
 
 /**
  * 场景
