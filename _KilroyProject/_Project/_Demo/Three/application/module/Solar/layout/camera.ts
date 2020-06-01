b56de84fae8125a00eda42910e56ec9dcf4407ff
kilroy_lyk <736587340@qq.com>
2020-06-01 18:42:44 +0800

    KILROY

--- _KilroyProject/_Project/_Demo/Three/application/module/Solar/layout/camera.ts
+++ _KilroyProject/_Project/_Demo/Three/application/module/Solar/layout/camera.ts
@@ -35,7 +35,7 @@ export default class Camera implements Layout {
         _this.instance = new THREE.PerspectiveCamera(
             60, Global.Function.getDomAspect(), 1, 40000
         );
-        _this.instance.position.set(0, 1500, 3000);
+        _this.instance.position.set(0, 500, 1500);
         
         _this.createController();
     }
