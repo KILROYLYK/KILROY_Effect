aa19769dffb824b9499dc41b2a0b2284c52b6a99
kilroy_lyk <736587340@qq.com>
2020-06-04 18:36:27 +0800

    KILROY

--- _KilroyProject/_Project/_Demo/Three/application/module/Car/component/light.ts
+++ _KilroyProject/_Project/_Demo/Three/application/module/Car/component/light.ts
@@ -2,6 +2,7 @@ import Global from '../../../constant/global';
 import Component from '../../../interface/component';
 
 import * as THREE from 'three';
+import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib';
 
 /**
  * 光源
@@ -13,6 +14,8 @@ export default class Light implements Component {
     
     private lightAmbient: THREE.AmbientLight = null; // 环境光源
     private lightDirectional: THREE.DirectionalLight = null; // 定向光源
+    private lightArea1: THREE.Group = null; // 区域光源1
+    private lightArea2: THREE.Group = null; // 区域光源2
     
     public instance: THREE.Group = null; // 实例
     
@@ -42,6 +45,7 @@ export default class Light implements Component {
         _this.instance.position.set(0, 0, 0);
         
         _this.createLight();
+        _this.createArea();
     }
     
     /**
@@ -51,8 +55,12 @@ export default class Light implements Component {
     private init(): void {
         const _this = this;
         
+        RectAreaLightUniformsLib.init();
+        
         _this.instance.add(_this.lightAmbient);
         _this.instance.add(_this.lightDirectional);
+        _this.instance.add(_this.lightArea1);
+        _this.instance.add(_this.lightArea2);
         _this.scene.add(_this.instance);
     }
     
@@ -99,5 +107,53 @@ export default class Light implements Component {
         _this.lightDirectional.shadow.camera.bottom = -distance;
         _this.lightDirectional.shadow.camera.far = 2000;
         _this.lightDirectional.shadow.bias = -0.0001;
+        
+        _this.lightArea1 = new THREE.Group();
+        _this.lightArea1.position.set(50, 40, 0);
+        _this.lightArea1.rotation.set(0, Math.PI / 2, 0);
+        
+        _this.lightArea2 = new THREE.Group();
+        _this.lightArea2.position.set(-50, 40, 0);
+        _this.lightArea2.rotation.set(0, -Math.PI / 2, 0);
+    }
+    
+    /**
+     * 创建区域
+     * @return {void}
+     */
+    private createArea(): void {
+        const _this = this;
+        
+        const light1 = new THREE.RectAreaLight(
+            0xffffff, 10, 50, 50
+        );
+        
+        const mesh1 = new THREE.Mesh(
+            new THREE.PlaneBufferGeometry(),
+            new THREE.MeshBasicMaterial({
+                side: THREE.BackSide
+            })
+        );
+        mesh1.scale.x = light1.width;
+        mesh1.scale.y = light1.height;
+        
+        light1.add(mesh1);
+        _this.lightArea1.add(light1);
+    
+        const light2 = new THREE.RectAreaLight(
+            0xffffff, 10, 50, 50
+        );
+    
+        const mesh2 = new THREE.Mesh(
+            new THREE.PlaneBufferGeometry(),
+            new THREE.MeshBasicMaterial({
+                side: THREE.BackSide
+            })
+        );
+        mesh2.scale.x = light2.width;
+        mesh2.scale.y = light2.height;
+    
+        mesh2.add(mesh2);
+        _this.lightArea2.add(mesh2);
     }
 }
