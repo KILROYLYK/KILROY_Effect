51b9efd852233c08c6f0c5af9ea28b99fa15b5c8
kilroy_lyk <736587340@qq.com>
2020-06-18 16:31:36 +0800

    KILROY

--- _KilroyProject/_Project/_Demo/Pixi/application/module/Parallax/stage.ts
+++ _KilroyProject/_Project/_Demo/Pixi/application/module/Parallax/stage.ts
@@ -5,6 +5,8 @@ import * as PIXI from 'pixi.js';
 
 import Loader from '../../controller/loader';
 
+import '../.././../static/css/Parallax/index.less';
+
 /**
  * 场景
  */
@@ -23,10 +25,15 @@ export default class Stage implements _Stage {
         ],
         data: null as object // 数据
     };
-    private readonly speed: number = 0.1; // 速度
+    private centerP: object = Global.Function.getDomCenter(); // 中心位置
+    private readonly speed: number = 0.05; // 速度
+    private readonly width: number = 1200; // 宽度
+    private readonly height: number = 800; // 高度
     private app: PIXI.Application = null; // 应用
     private container: PIXI.Container = null; // 容器
     private filter: PIXI.filters.DisplacementFilter = null; // 过滤器
+    private spriteB: PIXI.Sprite = null; // 背景
+    private spriteBS: PIXI.Sprite = null; // 背景阴影
     private component: object = { // 组件
     };
     private controller: object = { // 控制器
@@ -62,25 +69,24 @@ export default class Stage implements _Stage {
      */
     private create(): void {
         const _this = this,
-            resource = _this.resource.data,
-            spriteB = new PIXI.Sprite.from(resource.image_bg),
-            spriteS = new PIXI.Sprite.from(resource.image_bg_shadow);
+            resource = _this.resource.data;
         
         _this.app = new PIXI.Application({
-            width: 1500,
-            height: 1000,
+            width: _this.width,
+            height: _this.height,
             transparent: true
         });
+        
         _this.container = new PIXI.Container();
         
-        _this.app.stage.addChild(_this.container);
-        Global.Dom.appendChild(_this.app.view);
+        _this.spriteB = new PIXI.Sprite.from(resource.image_bg.texture);
+        _this.spriteB.position.set(-(_this.spriteB.width - _this.width) / 2, -(_this.spriteB.height - _this.height) / 2);
         
-        spriteS.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-        _this.filter = new PIXI.filters.DisplacementFilter(spriteS);
+        _this.spriteBS = new PIXI.Sprite.from(resource.image_bg_shadow.texture);
+        _this.spriteBS.position.set(-(_this.spriteBS.width - _this.width) / 2, -(_this.spriteBS.height - _this.height) / 2);
+        _this.spriteBS.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
         
-        _this.container.addChild(spriteB);
-        _this.container.addChild(spriteS);
+        _this.filter = new PIXI.filters.DisplacementFilter(_this.spriteBS);
     }
     
     /**
@@ -92,7 +98,14 @@ export default class Stage implements _Stage {
         
         _this.isInit = true;
         
-        Global.Dom.addEventListener('mousemove', _this.mouseMove.bind(_this), false)
+        _this.app.stage.addChild(_this.container);
+        
+        _this.container.addChild(_this.spriteB);
+        _this.container.addChild(_this.spriteBS);
+        
+        Global.Dom.appendChild(_this.app.view);
+        
+        Global.Document.addEventListener('mousemove', _this.animation.bind(_this), false);
     }
     
     /**
@@ -116,20 +129,31 @@ export default class Stage implements _Stage {
         const _this = this;
         
         if (!_this.isInit) return;
-        
     }
     
     /**
-     * 鼠标移动执行动画
+     * 动画
      * @param {MouseEvent} e 鼠标事件
      * @return {void}
      */
-    private mouseMove(e: MouseEvent): void {
+    private animation(e: MouseEvent): void {
         const _this = this,
-            centerP = Global.Function.getDomCenter(), // 中心位置
-            x = (Global.mouseP.x - centerP.x) * _this.speed,
-            y = (Global.mouseP.y - centerP.y) * _this.speed * 2;
+            TweenMax = Global.GSAP.TweenMax,
+            Sine = Global.GSAP.Sine,
+            x = (Global.mouseP.x - _this.centerP.x) * _this.speed,
+            y = (Global.mouseP.y - _this.centerP.y) * _this.speed;
         
-        
+        TweenMax
+            .to(Global.mouseP, 2, {
+                x: e.clientX,
+                y: e.clientY,
+                ease: Sine.easeOut,
+                onUpdate() {
+                    _this.container.filters = [ _this.filter ];
+                    _this.spriteB.position.set(x - (_this.spriteB.width - _this.width) / 2, y - (_this.spriteB.height - _this.height) / 2);
+                    _this.spriteBS.position.set(x - (_this.spriteB.width - _this.width) / 2, y - (_this.spriteB.height - _this.height) / 2);
+                    _this.filter.scale.set(-x, -y);
+                }
+            });
     }
 }
