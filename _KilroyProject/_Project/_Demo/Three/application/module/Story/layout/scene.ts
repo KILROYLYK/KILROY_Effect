071f37429d8fb920fc3ead55fdd9a7a98e4576d9
kilroy_lyk <736587340@qq.com>
2020-06-18 17:12:48 +0800

    KILROY

---
+++ _KilroyProject/_Project/_Demo/Three/application/module/Story/layout/scene.ts
@@ -0,0 +1,55 @@
+import Global from '../../../constant/global';
+import Layout from '../../../interface/layout';
+
+import * as THREE from 'three';
+
+/**
+ * 场景
+ */
+export default class Scene implements Layout {
+    public instance: THREE.Scene = null; // 实例
+    
+    /**
+     * 构造函数
+     * @constructor Scene
+     */
+    constructor() {
+        const _this = this;
+        
+        _this.create();
+        _this.init();
+    }
+    
+    /**
+     * 创建
+     * @return {void}
+     */
+    private create(): void {
+        const _this = this,
+            color = '#ffffff';
+        
+        _this.instance = new THREE.Scene();
+        _this.instance.background = new THREE.Color(color);
+        _this.instance.fog = new THREE.FogExp2(color, 0.0001);
+    }
+    
+    /**
+     * 初始化
+     * @return {void}
+     */
+    private init(): void {
+        const _this = this;
+    }
+    
+    /**
+     * 销毁
+     * @return {void}
+     */
+    public destroy(): void {
+        const _this = this;
+        
+        if (!_this.instance) return;
+        
+        _this.instance = null;
+    }
+}
