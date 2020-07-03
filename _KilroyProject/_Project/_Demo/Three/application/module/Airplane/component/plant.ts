dfd39b6242952d16b3e7014b4982d19971896ebb
kilroy_lyk <736587340@qq.com>
2020-07-03 18:33:06 +0800

    KILROY

--- _KilroyProject/_Project/_Demo/Three/application/module/Airplane/component/plant.ts
+++ _KilroyProject/_Project/_Demo/Three/application/module/Airplane/component/plant.ts
@@ -43,8 +43,6 @@ export default class Plant implements Component {
         _this.instance = new THREE.Group();
         _this.instance.name = _this.name;
         _this.instance.position.set(0, 0, 0);
-        
-        _this.createTree();
     }
     
     /**
@@ -75,7 +73,19 @@ export default class Plant implements Component {
         const _this = this,
             cycleS = 0.002; // 周期速度
         
-        if (Math.random() > 0.98) ;
+        if (Math.random() > 0.98) _this.createTree();
+        
+        _this.plant = _this.plant.filter((v, i, a) => {
+            v.cycle += cycleS;
+            v.object.position.x = Math.cos(v.cycle) * v.track;
+            v.object.position.y = Math.sin(v.cycle) * v.track;
+            v.object.rotation.z = -Math.PI / 2 + v.cycle;
+            if (v.cycle > Math.PI) {
+                _this.instance.remove(v.object);
+                return false;
+            }
+            return true;
+        });
     }
     
     /**
@@ -85,9 +95,8 @@ export default class Plant implements Component {
     private createTree(): void {
         const _this = this,
             track = 500, // 轨道
-            range = 350, // 范围
             height = 20,
-            scale = Global.Base.getRandomInt(1, 3) / 10,
+            scale = Global.Base.getRandomInt(2, 4) / 3,
             y = track + height * scale / 2;
         
         const trunkG = new THREE.BoxGeometry(10, height, 10),
@@ -96,24 +105,47 @@ export default class Plant implements Component {
                 flatShading: true
             }),
             trunk = new THREE.Mesh(trunkG, trunkM);
-        trunk.position.set(0, y, Global.Base.getRandomInt(-range, range));
+        trunk.position.set(0, 0, 0);
+        trunk.castShadow = true;
+        trunk.receiveShadow = true;
         
         const leavesG1 = new THREE.CylinderGeometry(
-            1, 12 * 3,
-            12 * 3, 4
+            1, 36,
+            36, 4
             ),
             leavesG2 = new THREE.CylinderGeometry(
-                1, 9 * 3,
-                9 * 3, 4
+                1, 27,
+                27, 4
             ),
             leavesG3 = new THREE.CylinderGeometry(
-                1, 6 * 3,
-                6 * 3, 4
-            );
+                1, 18,
+                18, 4
+            ),
+            leavesM = new THREE.MeshPhongMaterial({
+                color: '#157218',
+                flatShading: true
+            }),
+            leaves1 = new THREE.Mesh(leavesG1, leavesM),
+            leaves2 = new THREE.Mesh(leavesG2, leavesM),
+            leaves3 = new THREE.Mesh(leavesG3, leavesM);
+        leaves1.position.set(0, height / 2 + 36 / 2, 0);
+        leaves1.castShadow = true;
+        leaves1.receiveShadow = true;
+        leaves2.position.set(0, height / 2 + 36 * 0.6 + 27 / 2, 0);
+        leaves2.castShadow = true;
+        leaves2.receiveShadow = true;
+        leaves3.position.set(0, height / 2 + 36 * 0.6 + 27 * 0.7 + 18 / 2, 0);
+        leaves3.castShadow = true;
+        leaves3.receiveShadow = true;
         
         
         const tree = new THREE.Object3D();
+        tree.position.set(0, y, Global.Base.getRandomInt(-350, 500));
+        tree.scale.setScalar(scale);
         tree.add(trunk);
+        tree.add(leaves1);
+        tree.add(leaves2);
+        tree.add(leaves3);
         _this.plant.push({
             cycle: 0,
             track: y,
