ef2534bee66f63e55a3cfcb7b1a04ae52e1f0c99
kilroy_lyk <736587340@qq.com>
2021-03-02 18:30:31 +0800

    KILROY

--- _KilroyProject/_Project/_Demo/Web/application/module/PlantTree/stage.ts
+++ _KilroyProject/_Project/_Demo/Web/application/module/PlantTree/stage.ts
@@ -10,21 +10,32 @@ import '../../../resource/css/PlantTree/index.less';
 export default class Stage implements _Stage {
     private isInit: boolean = false; // 是否初始化
     private template: object = { // 模板对象
-        base: `<div id="box_tree" class="box_tree"></div>
+        base: `<div id="box_tree" class="box_tree">
+                <div class="tree tree_t"></div>
+                <div class="tree tree_1"></div>
+                <div class="tree tree_2"></div>
+                <div class="tree tree_3"></div>
+                <div class="tree tree_b"></div>
+            </div>
+            <div class="bg_bottom"></div>
             <div id="box_progress" class="box_progress">
                 <div class="progress_bar">
-                    <div class="text text_1">0/0</div>
+                    <div class="text t_1"><span>0 / 0</span></div>
                 </div>
             </div>
             <div id="box_water_info" class="box_water_info">
-                <div class="text"><span>已浇水：</span>0次</div>
-                <div class="text"><span>当前排名：</span>未上榜</div>
-                <div class="text">可浇水0次</div>
+                <div class="text t_1">已浇水：<span> 0 次</span></div>
+                <div class="text t_1">当前排名：<span>未上榜</span></div>
+                <div class="text t_1"><span>可浇水 0 次</span></div>
             </div>
-            <div id="button_water" class="button_water"></div>
-            <div id="button_explain" class="button_explain"></div>
-            <div id="button_share" class="button_share"></div>`,
-        addProgress: `<div class="progress_add text_1 icon_1">+1</div>`
+            <div id="button_water" class="button button_water"></div>
+            <div id="button_explain" class="button button_explain"></div>
+            <div id="button_share" class="button button_share"></div>`,
+        add: `<div class="progress_add t_1 i_1"><span>+1</span></div>`,
+        user: `<div class="box_user">
+                <div class="text t_1">昵称</div>
+                <div class="text t_1"><span class="i_1"> 0 次</span></div>
+            </div>`,
     };
     
     /**
